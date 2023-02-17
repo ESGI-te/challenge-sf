c5ee8091c3385370874aa76a743ed8c69d3ee28b
tlugat <teo.lugat@hetic.net>
2023-02-17 23:33:20 +0100

    ✨ Add openAi and Recipe generator services

---
+++ src/Service/OpenAIService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Service;
+
+use Orhanerday\OpenAi\OpenAi;
+
+class OpenAIService
+{
+    protected OpenAi $openAi;
+    public function __construct($openAiKey) {
+        $this->openAi = new OpenAi($openAiKey);
+    }
+
+    public function generateText(string $prompt): string {
+        return $this->openAi->completion([
+            'model' => 'text-davinci-003',
+            'prompt' => $prompt,
+            'temperature' => 0.9,
+            'max_tokens' => 3200,
+            'frequency_penalty' => 0,
+            'presence_penalty' => 0.6,
+        ]);
+    }
+
+    public function generateImage(string $prompt): string {
+        return $this->openAi->image([
+            "prompt" => $prompt,
+            "n" => 1,
+            "size" => "256x256",
+            "response_format" => "url",
+        ]);
+    }
+}

fb24c2826797da94a4280c6ebfe0f319cb666371
tlugat <teo.lugat@hetic.net>
2023-02-25 00:07:47 +0100

    ✨ Add generate recipe form style

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -11,12 +11,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Constraints\Range;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class RecipeType extends AbstractType
@@ -26,22 +27,46 @@ class RecipeType extends AbstractType
     }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $defaultDifficulty = $this->em->getRepository(RecipeDifficulty::class)->findOneBy([
+            'name' => 'Normal'
+        ]);
+        $defaultDuration = $this->em->getRepository(RecipeDuration::class)->findOneBy([
+            'name' => 'Normal'
+        ]);
         $builder
             ->add('difficulty', EntityType::class, [
                 'class' => RecipeDifficulty::class,
                 'choice_label' => 'name',
-            ])            ->add('nb_people')
+                'attr' => [
+                    'class' => 'difficulty'
+                ],
+                'data' => $defaultDifficulty
+            ])
             ->add('duration', EntityType::class, [
                 'class' => RecipeDuration::class,
                 'choice_label' => 'name',
-            ])
-            ->add('nb_people', RangeType::class, [
-                'label' => 'Nombre de personnes',
                 'attr' => [
+                    'class' => 'duration'
+                ],
+                'data' => $defaultDuration
+            ])
+            ->add('nb_people', NumberType::class, [
+                'label' => 'Nombre de personnes',
+                'html5' => true,
+                'attr' => [
+                    'value' => 1,
+                    'step' => 1,
                     'min' => 1,
                     'max' => 100,
-                    'step' => 1,
-                ]
+                    'arrow' => false
+                ],
+                'constraints' => [
+                    new Range([
+                        'min' => 1,
+                        'max' => 100,
+                        'notInRangeMessage' => 'La valeur doit être comprise entre {{ min }} et {{ max }}.',
+                    ]),
+                ],
             ]);
             $this->addIngredientsOfTypeField($builder);
             $this->getIngredients($builder);
@@ -60,7 +85,10 @@ class RecipeType extends AbstractType
                 'label' => $type->getName(),
                 'choice_label' => 'name',
                 'mapped' => false,
-                'required' => false
+                'required' => false,
+                'attr' => [
+                   'class' => 'ingredients',
+                ]
             ]);
         }
     }
